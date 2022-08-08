e1838ceb4576f0fff8eb5e0fc67e06897f2a5f73
LaoPeng <2779824672@qq.com>
2022-08-08 21:15:46 +0800

    选择排序

--- src/main/java/org/pjj/sort/SelectSort.java
+++ src/main/java/org/pjj/sort/SelectSort.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 public class SelectSort {
     public static void main(String[] args) {
         int[] arr = {5,7,6,9,3,10,2,11};
-        select(arr);
+        select2(arr);
         System.out.println(Arrays.toString(arr));//[2, 3, 5, 6, 7, 9, 10, 11]
     }
 
@@ -36,6 +36,34 @@ public class SelectSort {
             //如果没有flag, 那么arr[j]就是最小值呢, 那么其实不需要交换, 但是这里还是执行了 min,Jindex就是默认值, 这不是坏了事了
             //交换, 将最小值放在数组前面,
             if(flag) {
+                int temp = arr[i];
+                arr[i] = min;
+                arr[jIndex] = temp;
+            }
+        }
+    }
+
+
+    /**
+     * 发现select()中的flag变量其实不需要, jIndex完全可以起到它本身的作用外, 起到flag的作用
+     */
+    public static void select2(int[] arr) {
+        for(int i=0; i < arr.length - 1; i++) {//假设10个元素,则实际排序9轮, 也就是说9个元素都有序了, 则最后一个元素自然是有序的了, 所以 -1
+            int min = arr[i];
+            int jIndex = -1;
+
+            // j = i是因为 i表示第几轮排序, 第几轮排序表示排好了几个元素, i = 0表示一个元素都没排好序则从 arr[0] 开始选出最小值
+            // i = 5表示已经有5个元素排好序了, 则从 arr[5]开始选出最小值.
+            for(int j = i+1; j < arr.length; j++) {
+                if(arr[j] < min) {//选出最小值
+                    min = arr[j];//保存最小值
+                    jIndex = j;//保存最小值的下标
+                }
+            }
+
+            //jIndex != -1 说明需要交换
+            //交换, 将最小值放在数组前面,
+            if(jIndex != -1) {
                 int temp = arr[i];
                 arr[i] = min;
                 arr[jIndex] = temp;
