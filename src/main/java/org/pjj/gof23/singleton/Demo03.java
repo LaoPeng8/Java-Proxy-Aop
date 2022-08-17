17b9756897e2e1a69bb80a3491fa961ff8310a16
LaoPeng <2779824672@qq.com>
2022-08-17 11:16:51 +0800

    双重检测锁

--- src/main/java/org/pjj/gof23/singleton/Demo03.java
+++ src/main/java/org/pjj/gof23/singleton/Demo03.java
@@ -9,26 +9,20 @@ package org.pjj.gof23.singleton;
  */
 public class Demo03 {
 
-    private static Demo03 instance = null;
+    private static volatile Demo03 instance = null; //加入 volatile 防止指令重排, 导致单例出现问题
 
     private Demo03(){};
 
-    public Demo03 getInstance() {
-        if(instance == null) {
-            Demo03 demo03;
-            synchronized (Demo03.class) {
-                demo03 = instance;
-                if(demo03 == null) {
-                    synchronized (Demo03.class) {
-                        if(demo03 == null) {
-                            demo03 = new Demo03();
-                        }
-                    }
-                    instance = demo03;
+    public static Demo03 getInstance(){
+
+        if(instance == null){//该if是为了, 多线程时不用每个线程都进来等待锁(如果 instance已经初始化了, 则之后得线程就不用每次都等待锁了)
+            synchronized (Demo03.class){ // 加锁目的, 防止多线程同时进入造成对象多次实例化, 保证同时只有一个线程能进入
+                if(instance == null){
+                    instance = new Demo03() ;//不是一个原子性操作
                 }
             }
         }
-        return instance;
+        return instance ;
     }
 
 }
